fa866bb5772a1c41100d2e84040c87d84dc9684f
LoaTer <78078439+FALoater@users.noreply.github.com>
2022-05-19 21:44:20 +0100

    Create convertCase.py

---
+++ Questions/convertCase.py
@@ -0,0 +1,5 @@
+# create a program that asks the user for a letter
+# and then converts that letter to uppercase or lowercase, depending on the letter
+# also validate input, so that if the user doesn't enter a letter it will not run
+# also make sure that the user only enters 1 letter
+# for reference, "A" is 65 in ASCII and "a" is 97 in ASCII
